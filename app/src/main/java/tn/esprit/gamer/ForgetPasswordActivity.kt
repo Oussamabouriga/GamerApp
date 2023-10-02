320d6acae04c8d40b68be7ea969d80b8d51c8cd2
Oussamabouriga <mohamedbouriga.bouriga@esprit.tn>
2023-10-02 22:59:36 +0100

    controle de saisie signUp

--- app/src/main/java/tn/esprit/gamer/ForgetPasswordActivity.kt
+++ app/src/main/java/tn/esprit/gamer/ForgetPasswordActivity.kt
@@ -1,11 +1,88 @@
 package tn.esprit.gamer
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import tn.esprit.gamer.databinding.ActivityForgetPasswordBinding
 
 class ForgetPasswordActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityForgetPasswordBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forget_password)
+        binding = ActivityForgetPasswordBinding.inflate(layoutInflater,null,false)
+        setContentView(binding.root)
+
+        binding.forgetPassMailNumber.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                var forgetText = s?.toString()
+                if (forgetText != null && isItInt(forgetText) ) {
+                    phoneValidate(forgetText)
+                }else if(forgetText != null) {
+                    emailValidate(forgetText)
+                }
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                var forgetText = s?.toString()
+                if (forgetText != null && isItInt(forgetText) ) {
+                    phoneValidate(forgetText)
+                }else if(forgetText != null) {
+                    emailValidate(forgetText)
+                }
+            }
+
+
+                override fun afterTextChanged(s: Editable?) {
+                var forgetText = s?.toString()
+                if (forgetText != null && isItInt(forgetText) ) {
+                    phoneValidate(forgetText)
+                }else if(forgetText != null) {
+                    emailValidate(forgetText)
+                }
+                }
+        })
+
+
     }
+
+    fun isItInt(s: String): Boolean {
+        return try {
+            s.toInt()
+            true
+        } catch (e: NumberFormatException) {
+            false
+        }
+    }
+    private fun emailValidate(email: String): Boolean {
+        val emailRegex = "^[a-zA-Z0-9._%+-]+@esprit\\.tn\$".toRegex()
+        if(email.matches(emailRegex) && email.isNotEmpty()) {
+            binding.forgetPassMail.helperText = ""
+            return true
+        }else if(email.isEmpty()){
+            binding.forgetPassMail.helperText = "Must not be empty !"
+            return false
+
+        }
+        else{
+            binding.forgetPassMail.helperText = "Not the right format !"
+            return false
+        }
+    }
+        private fun phoneValidate(phone: String): Boolean {
+
+            if(phone.length >=8) {
+                binding.forgetPassMail.helperText = ""
+                return true
+            }else{
+                binding.forgetPassMail.helperText = "Minimum 8 numbers !"
+                return false
+            }
+        }
+
+
+
 }
--- app/src/main/java/tn/esprit/gamer/SignUpActivity.kt
+++ app/src/main/java/tn/esprit/gamer/SignUpActivity.kt
@@ -3,6 +3,8 @@ package tn.esprit.gamer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.google.android.material.snackbar.Snackbar
 import tn.esprit.gamer.databinding.ActivitySignUpBinding
 
@@ -18,6 +20,96 @@ class SignUpActivity : AppCompatActivity() {
 
         binding = ActivitySignUpBinding.inflate(layoutInflater,null,false)
         setContentView(binding.root)
+
+        binding.fullnameText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                val fullName = s?.toString()
+                if (fullName != null) {
+                    fullNameValidation(fullName)
+                }
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val fullName = s?.toString()
+                if (fullName != null) {
+                    fullNameValidation(fullName)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                val fullName = s?.toString()
+                if (fullName != null) {
+                    fullNameValidation(fullName)
+                }
+            }
+        })
+
+        binding.mailR.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                val email = s?.toString()
+                if (email != null) {
+                    emailValidateRegister(email)
+                }
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val email = s?.toString()
+                if (email != null) {
+                    emailValidateRegister(email)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                val email = s?.toString()
+                if (email != null) {
+                    emailValidateRegister(email)
+                }
+            }
+        })
+
+         binding.passwordR.addTextChangedListener(object : TextWatcher {
+             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                 val password = s?.toString()
+                 if (password != null) {
+                     passwordValidationRegister(password)                }
+             }
+
+             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                 val password = s?.toString()
+                 if (password != null) {
+                     passwordValidationRegister(password)                }
+             }
+
+             override fun afterTextChanged(s: Editable?) {
+                 val password = s?.toString()
+                 if (password != null) {
+                     passwordValidationRegister(password)
+                 }
+             }
+         })
+
+        binding.conPasswordR.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                val passwordc = s?.toString()
+                if (passwordc != null) {
+                    confPasswordValidationRegister(passwordc)                }
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val passwordc = s?.toString()
+                if (passwordc != null) {
+                    confPasswordValidationRegister(passwordc)                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                val passwordc = s?.toString()
+                if (passwordc != null) {
+                    confPasswordValidationRegister(passwordc)
+                }
+            }
+        })
+
+
         binding.regBtn.setOnClickListener {
             register()
         }
@@ -46,9 +138,14 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun fullNameValidation(name : String): Boolean {
-        if (name.length < 3 && name.isEmpty()) {
+        if (name.isEmpty()) {
+            binding.mailReg.helperText = "Must not be empty !"
             return false
         }
+        if(name.length < 3 ){
+            binding.mailReg.helperText = "Minimum 3 characters !"
+        }
+        binding.mailReg.helperText = ""
         return true
     }
 
@@ -56,47 +153,74 @@ class SignUpActivity : AppCompatActivity() {
     private fun emailValidateRegister(email: String): Boolean {
         val emailRegex = "^[a-zA-Z0-9._%+-]+@esprit\\.tn\$".toRegex()
         if(email.matches(emailRegex) && email.isNotEmpty()) {
-            binding.mailReg.helperText = null
+            binding.mailReg.helperText = ""
             return true
-        }else{
+        }else if(email.isEmpty()){
             binding.mailReg.helperText = "Must not be empty !"
             binding.mailReg.setPlaceholderText("@esprit.tn")
+            return false
+
+        }
+        else{
+            binding.mailReg.helperText = "Not the right format !"
             return false
         }
     }
 
 
     private fun passwordValidationRegister(pwd :String) :Boolean {
-        if (pwd.length < 8){
-            binding.passwordReg.helperText = " Must not be empty !"
-            return false
+            if (pwd.length < 8){
+                binding.passwordReg.helperText = "Minimum 8 characters !"
+                return false
+            }
+            if(pwd.isEmpty()){
+                binding.passwordReg.helperText = " Must not be empty !"
+            }
+            if(pwd.isNotEmpty()){
+                binding.passwordReg.helperText = ""
+
+            }
+            if (pwd.filter { it.isDigit() }.firstOrNull() == null) {
+                binding.passwordReg.helperText = " Must contain Numbers"
+                return false
+            }
+            if (pwd.filter { it.isLetter() }.filter { it.isUpperCase() }.firstOrNull() == null){
+                binding.passwordReg.helperText = " Must contain UpperCase"
+                return false
+            }
+            if (pwd.filter { it.isLetter() }.filter { it.isLowerCase() }.firstOrNull() == null) {
+                binding.passwordReg.helperText = " Must contain LowerCase"
+                return false
+            }
+            return true
         }
-
-        if (pwd.filter { it.isDigit() }.firstOrNull() == null)
-            return false
-        if (pwd.filter { it.isLetter() }.filter { it.isUpperCase() }.firstOrNull() == null)
-            return false
-        if (pwd.filter { it.isLetter() }.filter { it.isLowerCase() }.firstOrNull() == null)
-            return false
-
-        return true
-    }
 
         private fun confPasswordValidationRegister(pwd :String) :Boolean {
-        if (pwd.length < 8){
-            binding.conPasswordReg.helperText = " Must not be empty !"
-            return false
+            if (pwd.length < 8){
+                binding.conPasswordReg.helperText = "Minimum 8 characters !"
+                return false
+            }
+            if(pwd.isEmpty()){
+                binding.conPasswordReg.helperText = " Must not be empty !"
+            }
+            if(pwd.isNotEmpty()){
+                binding.conPasswordReg.helperText = ""
+
+            }
+            if (pwd.filter { it.isDigit() }.firstOrNull() == null) {
+                binding.conPasswordReg.helperText = " Must contain Numbers"
+                return false
+            }
+            if (pwd.filter { it.isLetter() }.filter { it.isUpperCase() }.firstOrNull() == null){
+                binding.conPasswordReg.helperText = " Must contain UpperCase"
+                return false
+            }
+            if (pwd.filter { it.isLetter() }.filter { it.isLowerCase() }.firstOrNull() == null) {
+                binding.conPasswordReg.helperText = " Must contain LowerCase"
+                return false
+            }
+            return true
         }
-
-        if (pwd.filter { it.isDigit() }.firstOrNull() == null)
-            return false
-        if (pwd.filter { it.isLetter() }.filter { it.isUpperCase() }.firstOrNull() == null)
-            return false
-        if (pwd.filter { it.isLetter() }.filter { it.isLowerCase() }.firstOrNull() == null)
-            return false
-
-        return true
-    }
 
 
 
